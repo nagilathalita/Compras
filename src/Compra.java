ce6ce31ac125f560c88275385190bad591288473
Nagila <nagilathalyta@hotmail.com>
2026-09-23 17:55:10 -0300

    Initial commit

---
+++ src/Compra.java
@@ -0,0 +1,29 @@
+public class Compra implements Comparable<Compra>{
+    private String descricao;
+    private double valor;
+
+    public Compra(String descricao, double valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra: descrição = " + descricao +
+                ", valor=" + valor;
+
+    }
+
+    @Override
+    public int compareTo(Compra outraCompra) {
+        return Double.valueOf(this.valor).compareTo(Double.valueOf(outraCompra.valor));
+    }
+}
